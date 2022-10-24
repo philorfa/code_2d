4f0aa8bfec9f82d2f9e396288f153ea07a725e08
philipposorf <philippos.orfanoudakis@metamaterial.com>
2022-10-24 12:57:53 +0300

    "Change frequencies"

--- Inverse/DBIM_MF_Fine.m
+++ Inverse/DBIM_MF_Fine.m
@@ -7,16 +7,16 @@ InP.new_res=input('Please enter a new coarse resolution on a mulitple of 0.5(mm)
 InP.linear_method=input('Please choose a linear method from TwIST, CGLS with L-curve, or LSQR (1, 2, or 3, etc.):');%%%%linear solver. check in inverse solver.m
 InP.model_phantom=input('Please choose the model nuber');%%%%for example 146
 % maximum total number of iterations
-total_freqs{1} = [0.9]*1e9; %frequency hopping 
-total_freqs{2} = [1.0]*1e9;
-total_freqs{3} = [1.1]*1e9;
-total_freqs{4} = [1.3]*1e9;
-total_freqs{5} = [1.4]*1e9;
+%total_freqs{1} = [0.9]*1e9; %frequency hopping 
+%total_freqs{2} = [1.0]*1e9;
+%total_freqs{3} = [1.1]*1e9;
+%total_freqs{4} = [1.3]*1e9;
+%total_freqs{5} = [1.4]*1e9;
 
 %total_freqs{1} = [0.5 0.8]*1e9; %GHz mulitple frequency 0.5 and 0.8
-%total_freqs{1} = [1]*1e9;%%%can be mixed with frequency hopping
+total_freqs{1} = [1]*1e9;%%%can be mixed with frequency hopping
 
-maxIter=[10 10 10 10 10];%%number of iterations for each total_freqs index.
+maxIter=[20];%%number of iterations for each total_freqs index.
 % check input, frequency, and path
 path = Check_Input(InP, total_freqs);
 
